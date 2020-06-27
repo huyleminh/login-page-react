f950f778204baecb7b6b1249d5d1572e49c82b6f
huyleminh <leminhhuy.hcmus@gmail.com>
2020-06-28 00:14:44 +0700

    App.tsx

---
+++ my-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Switch, 
+  Route
+} from "react-router-dom";
+import Manager from "./Layouts/Manager";
+import Information from "./Layouts/Information";
+import Products from "./Layouts/Products";
+import Login from "./Login";
+import DashBoard from "./Layouts/DashBoard";
+import PageNotFound from "./Errors/PageNotFound";
+import Forbidden from "./Errors/Forbidden";
+
+class App extends React.Component {
+  render() {
+    return(
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route path="/manager" component={Manager} />
+          <Route path="/information" component={Information} />
+          <Route path="/products" component={Products} />
+          <Route path="/forbidden" component={Forbidden} />
+          <Route exact path="/" component={DashBoard} />
+          <Route component={PageNotFound} />
+        </Switch>
+    );
+  }
+}
+
+export default App;
