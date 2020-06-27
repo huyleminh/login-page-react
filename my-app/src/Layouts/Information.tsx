efd305b860a06c2d00b24864ccca74e234381dfc
huyleminh <leminhhuy.hcmus@gmail.com>
2020-06-28 00:16:35 +0700

    Create Information.tsx

---
+++ my-app/src/Layouts/Information.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "../NavBar";
+import { IBaseProps } from "../BaseInterface/BaseInterface";
+import Authenticated from "../Errors/Authenticated"
+
+class Information extends React.Component<IBaseProps, any> {
+    render() {
+        return(
+            <div>
+                {Authenticated(this.props)}
+                <NavBar />
+                <h1>Information</h1>
+            </div>
+        );
+    }
+}
+
+export default Information; 
