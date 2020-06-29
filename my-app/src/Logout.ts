e1dbea315065818c93db89ca062fb8799352fbf8
huyleminh <leminhhuy.hcmus@gmail.com>
2020-06-29 14:38:11 +0700

    Update Logout.ts

--- my-app/src/Logout.ts
+++ my-app/src/Logout.ts
@@ -1,5 +1,8 @@
-const Logout = () => {
+import { IBaseProps } from "./BaseInterface/BaseInterface";
+
+const Logout = (props: IBaseProps) => {
     localStorage.clear()
+    props.history.push("/login")
 }
 
 export default Logout;
