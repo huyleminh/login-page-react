59b6c29cd8c8d765622d96c7eadc8e606647daf0
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-09 12:03:55 +0700

    Forbidden.tsx

--- my-app/src/Errors/Forbidden.tsx
+++ my-app/src/Errors/Forbidden.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "../assets/403.css";
-import NavBar from "../NavBar"
-import {IBaseProps} from "../BaseInterface/BaseInterface"
+import {IBaseProps} from "../BaseInterfaces/BaseInterface";
+
 
 
 class Forbidden extends React.Component<IBaseProps, any> {
+    goHome = () => {
+        this.props.history.push("/");
+    }
+
+    handleLogout = () => {
+        localStorage.clear()
+        this.props.history.push("/login")
+    }
+
     render() {
         return (
-            <div>
-                <NavBar />
-                <div className="bg-403"></div>
+            <div className="bg-403">
+                <button className="btn-error" onClick={this.goHome}>Trang chủ</button>
+                <button className="btn-error" onClick={this.handleLogout}>Đăng nhập</button>
             </div>
         )
     }
