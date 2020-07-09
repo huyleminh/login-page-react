cb15c1a9217b9239d37453693b4e9beafd32b2a8
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-09 12:05:09 +0700

    Update NavBar.tsx

--- my-app/src/NavBar.tsx
+++ my-app/src/NavBar.tsx
@@ -1,27 +1,48 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import "./assets/NavBar.css";
-import Logout from "./Logout"
-import { IBaseProps } from "./BaseInterface/BaseInterface";
+import { NavLink } from "react-router-dom";
+import "./assets/NavBar.css"
+import { IData } from "./BaseInterfaces/BaseInterface";
+import Data from "./Data/data.json"
 
+
+const role = localStorage.getItem('role');
+const data: IData = Data;
 
 class NavBar extends React.Component<any, any> {
     handleLogOut = () => {
         localStorage.clear();
     }
 
+    createTitle = (path: string) => {
+        return (path[1].toUpperCase() + path.slice(2, path.length))
+    }
+
     render() {
-        return (
-            <div>
-                <ul>
-                    <li> <NavLink className="nav-link" exact to="/" activeClassName="selected">Home</NavLink> </li>
-                    <li> <NavLink className="nav-link" to="/information" activeClassName="selected">Information</NavLink> </li>
-                    <li> <NavLink className="nav-link" to="/manager" activeClassName="selected">Manager</NavLink> </li>
-                    <li> <NavLink className="nav-link" to="/products" activeClassName="selected">Products</NavLink> </li>
-                    <li style={{ float: "right" }}> <NavLink className="nav-link" to="/login" onClick={this.handleLogOut}>Logout</NavLink></li>
-                </ul>
-            </div>
-        );
+        if(role !== null) {
+            const listItem = data[role].path.map((element: string) => {
+                if (element === "/")
+                    return (
+                        <li className="nav-li"> <NavLink id="nav-link" exact to="/" activeClassName="selected">Home</NavLink> </li>
+                    )
+                else if (element === "/login")
+                    return (
+                        <li className="nav-li"> <NavLink id="nav-link" to="/login" onClick={this.handleLogOut}>Log out</NavLink></li>
+                    )
+                else {
+                    return (
+                        <li className="nav-li" > <NavLink id="nav-link" exact to={element} activeClassName="selected">{this.createTitle(element)}</NavLink> </li>
+                    )
+                }
+            })
+            return (
+                <div className="layout-header"> <ul className="nav-container-right">{listItem}</ul> </div>
+            )
+        }
+        else {
+            return (
+                <div></div>
+            )
+        }
     }
 }
 
