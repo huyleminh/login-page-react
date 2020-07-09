db85fcb487e783a4f7db14aecd641ac03946a80a
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-09 12:04:41 +0700

    Update DashBoard.tsx

--- my-app/src/Layouts/DashBoard.tsx
+++ my-app/src/Layouts/DashBoard.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import NavBar from "../NavBar";
-import { IBaseProps } from "../BaseInterface/BaseInterface";
-import Authenticated from "../Errors/Authenticated"
+import { IBaseProps } from "../BaseInterfaces/BaseInterface";
 
 
 class DashBoard extends React.Component<IBaseProps, any> {
     render() {
         return (
             <div>
-                {Authenticated(this.props)} 
                 <NavBar />
-                <h1>WELLCOME</h1>
+                <h1>This is home page</h1>
             </div>
         )
     }
