1ac5867bc9ee3b9cb322c6258bac18f8f2a1add7
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-23 11:11:33 +0700

    Fix dynamic navigation bar

--- my-app/src/NavBar.tsx
+++ my-app/src/NavBar.tsx
@@ -1,49 +1,104 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./assets/NavBar.css"
-import { IData } from "./BaseInterfaces/BaseInterface";
-import Data from "./Data/data.json"
+import { IBaseProps, IDataElementBar } from "./BaseInterfaces/BaseInterface";
+import data from "./Data/data.json";
+import {withRouter} from "react-router"
 
 
-const role = localStorage.getItem('role');
-const data: IData = Data;
 
-class NavBar extends React.Component<any, any> {
-    handleLogOut = () => {
-        localStorage.clear();
+class NavBar extends React.Component<IBaseProps, any> {
+    goHome = () => {
+        this.props.history.push("/") 
     }
 
-    createTitle = (path: string) => {
-        return (path[1].toUpperCase() + path.slice(2, path.length))
+    handleLogOut = () => {
+        localStorage.clear();
+        this.props.history.push("/login")
+    }
+
+    componentDidMount = () => {
+        const user = localStorage.getItem('user');
+        let userObj = {role: "", id: ""}
+        if (user !== null) {
+            userObj = JSON.parse(user);   
+        }
+
+        let contents = [] as Array<any>;
+        function getData(data: Array<IDataElementBar>, contents: Array<any>) {
+            for (let i = 0; i < data.length; i++) {
+                const element = data[i];
+                if (element.role.includes(userObj.role) === false) {      
+                    continue
+                }
+                if (element.member.includes(userObj.id) === false)
+                    continue;
+
+                switch (element.type) {
+                    case "bar":
+                        contents.push(`<button class="nav-dropdown-btn">${element.title}</button>`)
+                        break;
+                    case "content":
+                        contents.push(`<a class="nav-dropdown-content-NavLink" href=${element.path}>${element.title}</a>`)
+                        break;
+                }
+
+                if (element.hasChild === true) {
+                    getData(element.child, contents)
+                }
+            }
+        }
+
+        //Call to get data from data.json
+        getData(data, contents);
+
+        //Filter button
+        const navBar = contents.filter((element) => {
+            if (element.indexOf("button") === 1)
+                return element
+        })
+
+        //Filter NavLink
+        const navContent = contents.filter((element) => {
+            if (element.indexOf("a") === 1)
+                return element
+        })
+
+        //Inner data to navigation bar
+        for (let i = 0; i < navBar.length; i++) {
+            let target = document.getElementById("nav-dropdown-" + `${i + 1}`)
+            if (target)
+                target.innerHTML = navBar[i] + `<div class="nav-dropdown-content" id="nav-dropdown-content-${i + 1}"></div>`
+
+            let targetContent = document.getElementById("nav-dropdown-content-" + `${i + 1}`) 
+            let token = navBar[i].slice(navBar[i].indexOf('>') + 1, navBar[i].indexOf('<', 2));
+            
+            const content = navContent.filter(element => {
+                if (element.includes(token.toLowerCase()))
+                    return element
+            })
+
+            if (targetContent)
+                targetContent.innerHTML = content.join("")
+        }
     }
 
     render() {
-        if(role !== null) {
-            const listItem = data[role].path.map((element: string) => {
-                if (element === "/")
-                    return (
-                        <li className="nav-li"> <NavLink id="nav-link" exact to="/" activeClassName="selected">Home</NavLink> </li>
-                    )
-                else if (element === "/login")
-                    return (
-                        <li className="nav-li"> <NavLink id="nav-link" to="/login" onClick={this.handleLogOut}>Log out</NavLink></li>
-                    )
-                else {
-                    return (
-                        <li className="nav-li" > <NavLink id="nav-link" exact to={element} activeClassName="selected">{this.createTitle(element)}</NavLink> </li>
-                    )
-                }
-            })
-            return (
-                <div className="layout-header"> <ul className="nav-container-right">{listItem}</ul> </div>
-            )
-        }
-        else {
-            return (
-                <div></div>
-            )
-        }
+        return(
+            <div className="nav-container">
+                <div className="nav-dropdown">
+                    <button className="nav-dropdown-btn" >Home</button>
+                </div>
+
+                <div className="nav-dropdown" id="nav-dropdown-1"></div>
+
+                <div className="nav-dropdown" id="nav-dropdown-2"></div>
+
+                <div className="nav-dropdown" id="nav-dropdown-3"></div>
+
+                <div className="nav-dropdown"><button className="nav-dropdown-btn" onClick={this.handleLogOut}>Logout</button></div>
+            </div>
+        )
     }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
