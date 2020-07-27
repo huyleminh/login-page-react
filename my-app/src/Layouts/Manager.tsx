3db9770f891b5907fe5bf1401c1a6ec47858ca2a
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-27 15:38:40 +0700

    Update Manager.tsx

--- my-app/src/Layouts/Manager.tsx
+++ my-app/src/Layouts/Manager.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import NavBar from "../NavBar";
+import {NavBar} from "../NavBar";
 import { IBaseProps } from "../BaseInterfaces/BaseInterface";
+import  {Switch} from "react-router-dom"
+import PrivateRoute from "../PrivateRoute"
 
 
 
@@ -9,10 +11,32 @@ class Manager extends React.Component<IBaseProps, any> {
         return(
             <div>
                 <NavBar />
-                <h1>Manager page</h1>
+                <Switch>
+                    <PrivateRoute exact path="/manager"> <ManagerHome/> </PrivateRoute>
+                    <PrivateRoute exact path="/manager/add-member"> <AddMember/> </PrivateRoute>
+                    <PrivateRoute exact path="/manager/delete-member"> <DeleteMember/> </PrivateRoute>
+                </Switch>
             </div>
         );
     }
 }
 
 export default Manager;
+
+export function ManagerHome() {
+    return(
+        <h1>Manager home page</h1>
+    );
+}
+
+export function AddMember() {
+    return(
+        <h1>Add member</h1>
+    );
+}
+
+export function DeleteMember() {
+    return(
+        <h1>Delete member</h1>
+    );
+}
