88c05795f4830ac36d746681375702065d7d66c2
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-27 15:37:46 +0700

    Update Information.tsx

--- my-app/src/Layouts/Information.tsx
+++ my-app/src/Layouts/Information.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import NavBar from "../NavBar";
+import {NavBar} from "../NavBar";
 import "../assets/NavBar.css";
 import { IBaseProps } from "../BaseInterfaces/BaseInterface";
+import {Switch} from "react-router-dom"
+import PrivateRoute from "../PrivateRoute"
 
 
 
@@ -10,10 +12,32 @@ class Information extends React.Component<IBaseProps, any> {
         return(
             <div>
                 <NavBar />
-                <h1>Information page</h1>
+                <Switch>
+                    <PrivateRoute exact path="/information"> <InformationHome/> </PrivateRoute>
+                    <PrivateRoute exact path="/information/view-notification"> <Notifications/> </PrivateRoute>
+                    <PrivateRoute exact path="/information/add-notification"> <AddNotifications/> </PrivateRoute>
+                </Switch>
             </div>
         );
     }
 }
 
 export default Information; 
+
+export function InformationHome() {
+    return(
+        <h1>Information home page</h1>
+    );
+}
+
+export function Notifications() {
+    return(
+        <h1>View notifications</h1>
+    );
+}
+
+export function AddNotifications() {
+    return(
+        <h1>Add notifications</h1>
+    );
+}
