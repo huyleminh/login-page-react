290e120fbaf136abb2b4fdcc21c207e87a6ed32a
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-27 15:38:57 +0700

    Update Products.tsx

--- my-app/src/Layouts/Products.tsx
+++ my-app/src/Layouts/Products.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import NavBar from "../NavBar";
+import {NavBar} from "../NavBar";
 import { IBaseProps } from "../BaseInterfaces/BaseInterface"
-
+import  {Switch} from "react-router-dom"
+import PrivateRoute from "../PrivateRoute"
 
 
 class Products extends React.Component<IBaseProps, any> {
@@ -9,10 +10,32 @@ class Products extends React.Component<IBaseProps, any> {
         return(
             <div>
                 <NavBar />
-                <h1>Products page</h1>
+                <Switch>
+                    <PrivateRoute exact path="/products"> <ProductsHome/> </PrivateRoute>
+                    <PrivateRoute exact path="/products/view-products"> <ViewProducts/> </PrivateRoute>
+                    <PrivateRoute exact path="/products/add-new"> <AddNew/> </PrivateRoute>
+                </Switch>
             </div>
         );
     }
 }
 
 export default Products;
+
+export function ProductsHome() {
+    return(
+        <h1>Products home page</h1>
+    );
+}
+
+export function ViewProducts() {
+    return(
+        <h1>View products</h1>
+    );
+}
+
+export function AddNew() {
+    return(
+        <h1>Add new product</h1>
+    );
+}
