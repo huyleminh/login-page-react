b0312bdea201970d8be9d209d3ecd967bff5a8e4
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-30 12:02:14 +0700

    Update NavBar

--- my-app/src/NavBar.tsx
+++ my-app/src/NavBar.tsx
@@ -22,43 +22,6 @@ class NavBarView extends React.Component<IBaseProps, any> {
     this.props.history.push("/login");
   };
 
-  // componentDidMount = () => {
-  //     //Call to get data from data.json
-  //     let contents = [] as Array<any>;
-  //     let paths = [] as Array<any>;
-  //     let ids = [] as Array<any>;
-  //     let roles = [] as Array<any>;
-  //     getData(data, contents, paths, ids, roles);
-
-  //     //Filter button
-  //     const navBar = contents.filter((element) => {
-  //         return (element.indexOf("button") === 1)
-  //     })
-
-  //     //Filter NavLink
-  //     const navContent = contents.filter((element) => {
-  //         return (element.indexOf("a") === 1)
-  //     })
-
-  //     //Inner data to navigation bar
-  //     for (let i = 0; i < navBar.length; i++) {
-  //         let target = document.getElementById("nav-dropdown-" + `${i + 1}`)
-  //         if (target) {
-  //             target.innerHTML = navBar[i] + `<div class="nav-dropdown-content" id="nav-dropdown-content-${i + 1}"></div>`
-  //         }
-
-  //         let targetContent = document.getElementById("nav-dropdown-content-" + `${i + 1}`)
-  //         let token = navBar[i].slice(navBar[i].indexOf('/>') + 2, navBar[i].indexOf('</'));
-
-  //         const content = navContent.filter(element => {
-  //                 return (element.includes(token.toLowerCase()))
-  //         })
-
-  //         if (targetContent)
-  //             targetContent.innerHTML = content.join("")
-  //     }
-  // }
-
   dynamicNavBar = (data: Array<IDataElementBar>) => {
     let result = data.map((item: IDataElementBar) => {
       if (item.role.includes(userObj.role)) {
