416a2b252c960d12495243f10552f2ffc5f4dd8c
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-30 09:59:55 +0700

    Fix Navbar

--- my-app/src/NavBar.tsx
+++ my-app/src/NavBar.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./assets/NavBar.css"
-import { IBaseProps } from "./BaseInterfaces/BaseInterface";
+import { IBaseProps, IDataElementBar } from "./BaseInterfaces/BaseInterface";
 import data from "./Data/data.json";
 import {withRouter} from "react-router"
-import getData from "./GetData"
+import { Link } from "react-router-dom";
+// import getData from "./GetData"
 
 
+const user = localStorage.getItem('user');
+let userObj = {role: "", id: ""}
+if (user !== null) {
+    userObj = JSON.parse(user);   
+}
 
 class NavBarView extends React.Component<IBaseProps, any> {
     goHome = () => {
@@ -17,41 +23,65 @@ class NavBarView extends React.Component<IBaseProps, any> {
         this.props.history.push("/login")
     }
 
-    componentDidMount = () => {
-        //Call to get data from data.json
-        let contents = [] as Array<any>;
-        let paths = [] as Array<any>;
-        let ids = [] as Array<any>;
-        let roles = [] as Array<any>;
-        getData(data, contents, paths, ids, roles);
+    // componentDidMount = () => {
+    //     //Call to get data from data.json
+    //     let contents = [] as Array<any>;
+    //     let paths = [] as Array<any>;
+    //     let ids = [] as Array<any>;
+    //     let roles = [] as Array<any>;
+    //     getData(data, contents, paths, ids, roles);
 
-        //Filter button
-        const navBar = contents.filter((element) => {
-            return (element.indexOf("button") === 1)
-        })
+    //     //Filter button
+    //     const navBar = contents.filter((element) => {
+    //         return (element.indexOf("button") === 1)
+    //     })
         
-        //Filter NavLink
-        const navContent = contents.filter((element) => {
-            return (element.indexOf("a") === 1)
-        })
+    //     //Filter NavLink
+    //     const navContent = contents.filter((element) => {
+    //         return (element.indexOf("a") === 1)
+    //     })
         
-        //Inner data to navigation bar
-        for (let i = 0; i < navBar.length; i++) {
-            let target = document.getElementById("nav-dropdown-" + `${i + 1}`)
-            if (target) {
-                target.innerHTML = navBar[i] + `<div class="nav-dropdown-content" id="nav-dropdown-content-${i + 1}"></div>`
-            }
+    //     //Inner data to navigation bar
+    //     for (let i = 0; i < navBar.length; i++) {
+    //         let target = document.getElementById("nav-dropdown-" + `${i + 1}`)
+    //         if (target) {
+    //             target.innerHTML = navBar[i] + `<div class="nav-dropdown-content" id="nav-dropdown-content-${i + 1}"></div>`
+    //         }
 
-            let targetContent = document.getElementById("nav-dropdown-content-" + `${i + 1}`) 
-            let token = navBar[i].slice(navBar[i].indexOf('/>') + 2, navBar[i].indexOf('</'));
+    //         let targetContent = document.getElementById("nav-dropdown-content-" + `${i + 1}`) 
+    //         let token = navBar[i].slice(navBar[i].indexOf('/>') + 2, navBar[i].indexOf('</'));
             
-            const content = navContent.filter(element => {
-                    return (element.includes(token.toLowerCase()))
-            })
+    //         const content = navContent.filter(element => {
+    //                 return (element.includes(token.toLowerCase()))
+    //         })
 
-            if (targetContent)
-                targetContent.innerHTML = content.join("")
-        }
+    //         if (targetContent)
+    //             targetContent.innerHTML = content.join("")
+    //     }
+    // }
+
+    dynamiNavBar = (data: Array<IDataElementBar>) => {
+        data.map((item: IDataElementBar) => {
+            if (!item.role.includes(userObj.role))
+                return;
+            
+            if (!item.member.includes(userObj.id))
+                return;
+
+            if (item.hasChild) {
+                return(
+                    <div className="nav-dropdown">
+                        <button className="nav-dropdown-btn"><Link className="nav-dropdown-btn-a" to={item.path}/>${item.title}</button>
+                        {this.dynamiNavBar(item.child)}
+                    </div>
+                )
+            }
+            else {
+                return(
+                    <Link className="nav-dropdown-content-NavLink" to={item.path}>${item.title}</Link>
+                )
+            }
+        })
     }
 
     render() {
@@ -61,11 +91,15 @@ class NavBarView extends React.Component<IBaseProps, any> {
                     <button className="nav-dropdown-btn" onClick={this.goHome}>Home</button>
                 </div>
 
-                <div className="nav-dropdown" id="nav-dropdown-1" ></div>
+                {/* <div className="nav-dropdown" id="nav-dropdown-1" >
+                                      
+                </div>
 
                 <div className="nav-dropdown" id="nav-dropdown-2" ></div>
 
-                <div className="nav-dropdown" id="nav-dropdown-3" ></div>
+                <div className="nav-dropdown" id="nav-dropdown-3" ></div> */}
+
+                {this.dynamiNavBar(data)}
 
                 <div className="nav-dropdown"><button className="nav-dropdown-btn" onClick={this.handleLogOut}>Logout</button></div>
             </div>
