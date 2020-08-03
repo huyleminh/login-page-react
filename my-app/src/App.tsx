e2546bb2115ebd3609dd0b6f8e59e890046538ae
huyleminh <leminhhuy.hcmus@gmail.com>
2020-08-03 16:45:43 +0700

    Add Routes and components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,26 +3,46 @@ import {
   Switch, 
   Route
 } from "react-router-dom";
+//Manager components
 import Manager from "./Layouts/Manager";
+import AddMember from "./Layouts/AddMember";
+import DeleteMember from "./Layouts/DeleteMember";
+//Information components
 import Information from "./Layouts/Information";
+import Notification from "./Layouts/Notification";
+import AddNotification from "./Layouts/AddNotification";
+//Products components
 import Products from "./Layouts/Products";
+import AddProduct from "./Layouts/AddProduct";
+import ViewProducts from "./Layouts/ViewProducts";
+
 import Login from "./Layouts/Login";
 import DashBoard from "./Layouts/DashBoard";
 import PageNotFound from "./Errors/PageNotFound";
 import Forbidden from "./Errors/Forbidden";
-import PrivateRoute from "./PrivateRoute";
+
 
 class App extends React.Component<any, any> {
   render() {
     return(
         <Switch>
-          <Route path="/login" component={Login} />
-          <PrivateRoute path="/manager" component={Manager} />
-          <PrivateRoute path="/information" component={Information} />
-          <PrivateRoute path="/products" component={Products} />
-          <Route path="/403" component={Forbidden} />
-          <PrivateRoute exact path="/" component={DashBoard} />
-          <Route component={PageNotFound} />
+          <Route path="/login" component={Login}/>
+
+          <Route exact path="/manager" component={Manager}/>
+          <Route path="/manager/add-member" component={AddMember}/>
+          <Route path="/manager/delete-member" component={DeleteMember}/> 
+
+          <Route exact path="/information" component={Information}/>
+          <Route path="/information/view-notification" component={Notification}/>
+          <Route path="/information/add-notification" component={AddNotification}/>
+
+          <Route exact path="/products" component={Products}/>
+          <Route path="/products/add-new" component={AddProduct}/>
+          <Route path="/products/view-products" component={ViewProducts}/>
+
+          <Route path="/403" component={Forbidden}/>
+          <Route exact path="/" component={DashBoard}/>
+          <Route component={PageNotFound}/>
         </Switch>
     );
   }
