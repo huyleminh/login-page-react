5b6799231e840b85da20279aed1c066b2541c79f
huyleminh <leminhhuy.hcmus@gmail.com>
2020-08-05 15:29:57 +0700

    Fix

--- my-app/src/Layouts/DashBoard.tsx
+++ my-app/src/Layouts/DashBoard.tsx
@@ -4,6 +4,13 @@ import { IBaseProps } from "../BaseInterfaces/BaseInterface";
 
 
 class DashBoard extends React.Component<IBaseProps, any> {
+    componentWillMount() {
+        const user = localStorage.getItem("user");
+        if (user === null) {
+            this.props.history.push("/login")
+        }
+    }
+
     render() {
         return (
             <div>
