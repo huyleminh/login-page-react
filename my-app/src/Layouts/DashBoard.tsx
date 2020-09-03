0f2cfc7baac02d9dc90f0c0025db29da0fe7dbf6
huyleminh <leminhhuy.hcmus@gmail.com>
2020-09-03 11:30:14 +0700

    Update home

--- my-app/src/Layouts/DashBoard.tsx
+++ my-app/src/Layouts/DashBoard.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import {NavBar} from "../NavBar";
 import { IBaseProps } from "../BaseInterfaces/BaseInterface";
-import {LoginEvent } from "../events";
 
 
 class DashBoard extends React.Component<IBaseProps, any> {
-    componentWillMount() {
-        const user = localStorage.getItem("user");
-        if (user === null) {
-            LoginEvent.Init.baseEmit("login", this.props)
-        }
-    }
-
     render() {
         return (
             <div>
