b70e06f2a89faaff5957b05370b748fec1a4a315
huyleminh <leminhhuy.hcmus@gmail.com>
2020-09-03 11:28:07 +0700

    Add ValidatedAddForm event

--- my-app/src/events.ts
+++ my-app/src/events.ts
@@ -1,38 +1,40 @@
-import EventEmitter from "events"
+import EventEmitter from "events";
 
 export abstract class BaseEvent {
-    protected readonly _event : EventEmitter;
-    protected _listeners: any[]
-    
-    constructor() {
-        this._event = new EventEmitter();
-        this._listeners = []
-    }
+  protected readonly _event: EventEmitter;
+  protected _listeners: any[];
 
-    public baseEmit(eventName : string, data : any) {
-        this._event.emit(eventName, data)
-    }
+  constructor() {
+    this._event = new EventEmitter();
+    this._listeners = [];
+  }
 
-    public baseOn(eventName : string, data : any) {
-        const item = {name: eventName, data: data};
-        if (!this._listeners.includes(item))
-            this._listeners.push(item);
-        this._event.on(eventName, data)
-    }
+  public baseEmit(eventName: string, data?: any) {
+    this._event.emit(eventName, data);
+  }
 
-    public baseListenerCount(eventName: string) {
-        const events = this._listeners.filter(item => {
-            return (item.name === eventName)
-        })
-        return events.length
-    }
-    
+  public baseOn(eventName: string, data?: any) {
+    const item = { name: eventName, data: data };
+    if (!this._listeners.includes(item)) this._listeners.push(item);
+    this._event.on(eventName, data);
+  }
+
+  public baseListenerCount(eventName: string) {
+    const events = this._listeners.filter((item) => {
+      return item.name === eventName;
+    });
+    return events.length;
+  }
 }
 
 export class GlobalEvent extends BaseEvent {
-    public static Init = new GlobalEvent();
+  public static Init = new GlobalEvent();
 }
 
-export class LoginEvent extends BaseEvent{
-    public static Init = new LoginEvent();
+export class LoginEvent extends BaseEvent {
+  public static Init = new LoginEvent();
+}
+
+export class ValidatedAddForm extends BaseEvent {
+  public static Init = new ValidatedAddForm();
 }
